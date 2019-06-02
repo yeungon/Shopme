07be719c5a7519e398b4139604aac7e9f1f4e9de
Vuong Nguyen <yeungon@gmail.com>
2019-06-02 01:04:05 -0600

    first components

---
+++ src/components/TodoItems.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class TodoItem extends Component{
+    
+    return(){
+        return ()
+        <div>
+            Hello Todo item
+        </div>
+        )
+
+    }
+    
+}
+
+export default TodoItem;
