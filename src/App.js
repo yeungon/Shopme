1d9e8230ff13020692f0face3904ad5b22f93822
Vuong Nguyen <yeungon@gmail.com>
2019-06-02 01:03:52 -0600

    update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import TodoItems from './components/TodoItems';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
        <h3> Vuong's todo list </h3>
+       <TodoItem/>
+
       </div>
     );
   }
