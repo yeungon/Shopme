e1dbd3210ffdf1c8048209794d5316ed5d1c287a
Vuong Nguyen <yeungon@gmail.com>
2019-06-02 03:55:14 -0600

    data object array

---
+++ src/Vuongtasks.js
@@ -0,0 +1,35 @@
+const Vuongtask = [
+    {
+        id: 1,
+        text: "Take out the trash",
+        completed: true,
+        result: "done"
+    },
+    {
+        id: 2,
+        text: "Grocery shopping",
+        completed: false,
+        result: "done"
+    },
+    {
+        id: 3,
+        text: "Clean gecko tank",
+        completed: false,
+        result: "done"
+    },
+    {
+        id: 4,
+        text: "Mow lawn",
+        completed: true,
+        result: "done"
+    },
+    {
+        id: 5,
+        text: "Catch up on Arrested Development hhjhu",
+        completed: false,
+        result: "done"
+    
+    }
+]
+
+export default Vuongtask
