d1f7a519539d3e3599da8d089d60bcd28a419785
Vuong Nguyen <yeungon@gmail.com>
2019-06-02 03:54:39 -0600

    update app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+import Vuongtask from './Vuongtasks';
 
 import TodoItems from './components/TodoItems';
 
+
+class Headercomponent extends Component{
+  render(){
+    return (
+      <div>
+        <h2>Vuong 's Todo list App</h2>
+      </div>
+    );
+  }
+}
+
+
 class App extends Component {
+
+  constructor(){
+    super();
+    
+    this.todoItem = Vuongtask;
+
+    this.todoItem = this.todoItem.map((item, index) => <TodoItems key = {index} task = {item.text} completed = {item.completed} result = {item.result}/>);
+    
+  }
+  
   render() {
     return (
       <div className="App">
-       <h3> Vuong's todo list </h3>
-      <p> wwhat?</p>
-      
-       <TodoItems/>
+        <Headercomponent/> 
+                
+        {this.todoItem}
+       
 
       </div>
     );
