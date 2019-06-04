bd994de442804eab55e2b4c74e764a8338f2c56b
Vuong Nguyen <yeungon@gmail.com>
2019-06-04 05:36:44 -0600

    update data

--- src/Vuongtasks.js
+++ src/Vuongtasks.js
@@ -2,31 +2,31 @@ const Vuongtask = [
     {
         id: 1,
         text: "Writing the three last chapters",
-        completed: true,
+        completed: false,
         result: "on going"
     },
     {
         id: 2,
         text: "Applying for the Fullstack Web position",
-        completed: true,
+        completed: false,
         result: "on going"
     },
     {
         id: 3,
         text: "Mastering ReactJs and likely React Native",
-        completed: true,
+        completed: false,
         result: "on going"
     },
     {
         id: 4,
         text: "NodeJS, Nextjs && MERN stack",
-        completed: true,
+        completed: false,
         result: "on going"
     },
     {
         id: 5,
         text: "Mastering WordPress",
-        completed: false,
+        completed: true,
         result: "done"
     
     }
