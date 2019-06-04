6833bd2f2e4c838b30c7ed7347209d758e0b0008
Vuong Nguyen <yeungon@gmail.com>
2019-06-04 06:27:54 -0600

    remove the tag result, using bool

--- src/Vuongtasks.js
+++ src/Vuongtasks.js
@@ -3,31 +3,31 @@ const Vuongtask = [
         id: 1,
         text: "Writing the three last chapters",
         completed: false,
-        result: "on going"
+        
     },
     {
         id: 2,
         text: "Applying for the Fullstack Web position",
         completed: false,
-        result: "on going"
+        
     },
     {
         id: 3,
         text: "Mastering ReactJs and likely React Native",
         completed: false,
-        result: "on going"
+        
     },
     {
         id: 4,
         text: "NodeJS, Nextjs && MERN stack",
         completed: false,
-        result: "on going"
+        
     },
     {
         id: 5,
         text: "Mastering WordPress",
         completed: true,
-        result: "done"
+        
     
     }
 ]
